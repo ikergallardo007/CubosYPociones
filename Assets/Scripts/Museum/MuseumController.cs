d0921e85c484bdb48546650ec37f4dbe06100cbe
Iker Gallardo Arrieta <ikergallardo22@gmail.com>
2026-02-04 17:23:55 +0100

    Space button input implemented
    NS: Get instantiation positions.

--- Assets/Scripts/Museum/MuseumController.cs
+++ Assets/Scripts/Museum/MuseumController.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MuseumController : MonoBehaviour
 {
+    // Public Attributes
+    public GameObject cubePrefab;
+
+    // Private Properties
+    private InputAction collectAction;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
+        // Get the collect action from the Input System
+        collectAction = InputSystem.actions.FindAction("Collect");
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        bool collectPress = collectAction.WasPressedThisFrame();
+        if (collectPress)
+        {
+            print("Collect Button Pressed");
+        }
+    }
+
+    void CollectCube(Vector3 position)
+    {
+        Instantiate(cubePrefab, position, Quaternion.identity);
     }
 }
