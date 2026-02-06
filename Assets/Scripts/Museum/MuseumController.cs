8db1399d9c5d334cd132a558090c97b235f0fed8
Iker Gallardo Arrieta <ikergallardo22@gmail.com>
2026-02-06 18:01:18 +0100

    List of generated cubed to save implemented
    NS: Save to a file.

--- Assets/Scripts/Museum/MuseumController.cs
+++ Assets/Scripts/Museum/MuseumController.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -11,6 +12,7 @@ public class MuseumController : MonoBehaviour
     private InputAction collectAction; // Input action for collecting cubes
     private int i = 0; // Counter for the x-axis position of the cube
     private int j = 0; // Counter for the z-axis position of the cube
+    private int counter = 0; // Counter for the number of cubes collected
     private Color[] cubeColors = new Color[] {
                 Color.black,
                 Color.blue,
@@ -24,6 +26,8 @@ public class MuseumController : MonoBehaviour
                 Color.white,
                 Color.yellow
     };
+    [SerializeField]
+    private List<CubeObject> collectedCubes = new List<CubeObject>(); // List to keep track of collected cubes
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
@@ -39,8 +43,8 @@ public class MuseumController : MonoBehaviour
         bool collectPress = collectAction.WasPressedThisFrame();
         if (collectPress)
         {
-            print("Collect Button Pressed");
-            CollectCube(i, j);
+            counter++;
+            CollectCube(i, j, counter);
             j++;
             if (j == 16)
             {
@@ -54,10 +58,12 @@ public class MuseumController : MonoBehaviour
         }
     }
 
-    private void CollectCube(int i, int j)
+    private void CollectCube(int i, int j, int counter)
     {
         Vector3 position = new Vector3(i * 2, 0, j * 2); // Example position for the cube
-        CubeObject cube = Instantiate(cubePrefab, position, Quaternion.identity);
-        cube.cubeMeshRenderer.material.color = cubeColors[Random.Range(0, cubeColors.Length)];
+        CubeObject cube = Instantiate(cubePrefab, position, Quaternion.identity); // Instantiate the cube at the specified position with no rotation
+        cube.cubeMeshRenderer.material.color = cubeColors[Random.Range(0, cubeColors.Length)]; // Assign a random color to the cube from the predefined array
+        cube.name = "Cube_" + counter; // Name the cube with a unique identifier
+        collectedCubes.Add(cube); // Add the collected cube to the list
     }
 }
